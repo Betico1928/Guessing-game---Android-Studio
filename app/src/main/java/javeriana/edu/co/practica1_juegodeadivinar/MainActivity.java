ff67552bbe036969e7c77b19bba7c0f2fee870a4
Betico1928 <betoveluis@gmail.com>
2023-02-12 15:09:25 -0500

    Llamado del número por medio de Binding

--- app/src/main/java/javeriana/edu/co/practica1_juegodeadivinar/MainActivity.java
+++ app/src/main/java/javeriana/edu/co/practica1_juegodeadivinar/MainActivity.java
@@ -2,28 +2,29 @@ package javeriana.edu.co.practica1_juegodeadivinar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.EditText;
 
 import java.util.Random;
 
 import javeriana.edu.co.practica1_juegodeadivinar.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     ActivityMainBinding binding;
 
     // Creación del Random
     Random randomNumber = new Random();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -34,14 +35,21 @@ public class MainActivity extends AppCompatActivity {
         // Contador de intentos:
         int contadorDeIntentos = 0;
 
+
         binding.botonAdivinar.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick(View view)
+            public void onClick(View vistaInicial)
             {
                 Log.i("Numero a adivinar: ", String.valueOf(resultadoDelRandom));
 
-                //if (resultadoDelRandom != )
+                // Pasar del EditText al editable usando binding
+                Editable numeroRecibido = binding.textoNumeroAdivinado.getText();
+                int numeroSugerido = Integer.parseInt(numeroRecibido.toString());
+
+                Log.i("Numero sugerido: ", String.valueOf(numeroSugerido));
+
+                //if (resultadoDelRandom != numeroSugerido);
             }
         });
     }
