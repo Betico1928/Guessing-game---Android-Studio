d5ef5ddab57e3502b172378e06de9c70bba040e6
Betico1928 <betoveluis@gmail.com>
2023-02-12 16:05:19 -0500

    Cambios en la interfaz.

--- app/src/main/java/javeriana/edu/co/practica1_juegodeadivinar/MainActivity.java
+++ app/src/main/java/javeriana/edu/co/practica1_juegodeadivinar/MainActivity.java
@@ -2,12 +2,10 @@ package javeriana.edu.co.practica1_juegodeadivinar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.Random;
